7b2dfdcb8581bf65541385f8cc182eba35c28a19
laurent <lmazare@gmail.com>
2019-07-24 21:49:39 -0400

    Bugfix the ultrasonic sensor code.

--- examples/awr.ml
+++ examples/awr.ml
@@ -16,25 +16,20 @@ end = struct
   let create () =
     let tr = Gpio.create ~channel:23 ~mode:`board in
     let ec = Gpio.create ~channel:24 ~mode:`board in
-    Gpio.setup tr `output `off;
     Gpio.output tr 0;
+    Gpio.setup tr `output `off;
     Gpio.setup ec `input `off;
     { tr; ec }
 
-  let rec loop_until ~f =
-    if f ()
-    then ()
-    else (
-      Unix.sleepf 5e-6;
-      loop_until ~f)
+  let rec loop_until ~f = if f () then () else loop_until ~f
 
   let get t =
     Gpio.output t.tr 1;
     Unix.sleepf 15e-6;
     Gpio.output t.tr 0;
-    loop_until ~f:(fun () -> Gpio.input t.ec = 0);
-    let start_time = Unix.gettimeofday () in
     loop_until ~f:(fun () -> Gpio.input t.ec <> 0);
+    let start_time = Unix.gettimeofday () in
+    loop_until ~f:(fun () -> Gpio.input t.ec == 0);
     let stop_time = Unix.gettimeofday () in
     (stop_time -. start_time) *. 340. /. 2.
 
